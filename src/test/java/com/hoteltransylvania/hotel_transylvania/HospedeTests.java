50769675bccf571508823d3e1e747f9f15ee51eb
Alerreandro <alerreandrocosta@gmail.com>
2025-04-09 18:45:44 -0300

    Correções

--- src/test/java/com/hoteltransylvania/hotel_transylvania/HospedeTests.java
+++ src/test/java/com/hoteltransylvania/hotel_transylvania/HospedeTests.java
@@ -15,15 +15,13 @@ import com.hotel_transylvania.exceptions.UsuarioJaExisteException;
 import com.hotel_transylvania.services.UsuarioServices;
 
 @SpringBootTest(classes = HotelTransylvaniaApplication.class)
-@Transactional // Garante rollback após cada teste
+@Transactional 
 public class HospedeTests {
 
     @Autowired
     private UsuarioServices usuarioServices;
 
-    /**
-     * Testa a criação bem-sucedida de um Hospede com dados válidos.
-     */
+
     @Test
     void testCriarHospedeComSucesso() {
         HospedeDTO hospedeDto = new HospedeDTO();
@@ -38,9 +36,7 @@ public class HospedeTests {
         assertEquals("12345678901", hospedeCriado.getCpf());
     }
 
-    /**
-     * Testa se o cadastro lança exceção ao informar um CPF inválido.
-     */
+
     @Test
     void testCriarHospedeCpfInvalido() {
         HospedeDTO hospedeDto = new HospedeDTO();
@@ -54,9 +50,7 @@ public class HospedeTests {
         });
     }
 
-    /**
-     * Testa se o cadastro lança exceção ao tentar criar um Hospede com um CPF já cadastrado.
-     */
+  
     @Test
     void testCriarHospedeUsuarioJaExiste() {
         HospedeDTO hospedeDto = new HospedeDTO();
@@ -65,11 +59,9 @@ public class HospedeTests {
         hospedeDto.setTelefone("11999992222");
         hospedeDto.setEmail("pedro@example.com");
 
-        // Primeiro cadastro deve ser realizado com sucesso.
         Hospede hospedeCriado = usuarioServices.cadastrarHospede(hospedeDto);
         assertNotNull(hospedeCriado.getId());
 
-        // Segunda tentativa com o mesmo CPF deverá lançar exceção.
         assertThrows(UsuarioJaExisteException.class, () -> {
             usuarioServices.cadastrarHospede(hospedeDto);
         });
